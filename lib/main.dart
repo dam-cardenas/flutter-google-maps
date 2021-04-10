df21ca7747880d6a38e903d33386fed1697e3b0a
Damian Cardenas Q <damian.cardenas.developer@gmail.com>
2021-04-10 00:19:52 -0500

    permissions and http added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(TestApp());
@@ -18,84 +19,177 @@ class _TestAppState extends State<TestApp> {
   LocationData _locationData;
   bool _serviceEnabled;
   LatLng l;
-  var zoom = 16.0;
+  double zoom = 14.0;
+  double radius = 1000;
   MapController mc = new MapController();
 
+  //Widget mainWidget
   @override
   void initState() {
     super.initState();
-    getPermissions();
-
-    marker.onLocationChanged.listen((LocationData currentLocation) {
-      setState(() {
-        l = new LatLng(currentLocation.latitude, currentLocation.longitude);
-        if (mc != null) {
-          mc.move(l, zoom);
-        }
-        print(l);
-      });
+    getLocationPermissions().then((enabled) {
+      if (_permissionGranted == PermissionStatus.granted) {
+        // show map
+        // start with location service
+        marker.onLocationChanged.listen((LocationData currentLocation) {
+          setState(() {
+            l = new LatLng(currentLocation.latitude, currentLocation.longitude);
+            if (mc != null) {
+              try {
+                mc.move(l, zoom);
+              } catch (e) {
+                print(e);
+              }
+            }
+            print(l);
+          });
+        });
+      }
     });
   }
 
-  void getPermissions() async {
-    _serviceEnabled = await marker.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await marker.requestService();
-      if (!_serviceEnabled) {
-        return;
+  Future<bool> getLocationPermissions() async {
+    try {
+      _serviceEnabled = await marker.serviceEnabled();
+      while (!_serviceEnabled) {
+        _serviceEnabled = await marker.requestService();
+        setState(() {});
       }
-    }
-    _permissionGranted = await marker.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await marker.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
+      _permissionGranted = await marker.hasPermission();
+      while (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await marker.requestPermission();
+        setState(() {});
       }
+      return true;
+    } catch (e) {
+      return false;
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget loader() {
+    return Center(
+      child: Text('Loading...'),
+    );
+  }
+
+  Widget mapWidget() {
     var circleMarkers = <CircleMarker>[
       CircleMarker(
           point: l,
           color: Colors.blue.withOpacity(0.7),
           borderStrokeWidth: 2,
           useRadiusInMeter: true,
-          radius: 2000 // 2000 meters | 2 km
+          radius: radius // meassure in meters
           ),
     ];
+    return Center(
+      child: FlutterMap(
+        mapController: mc,
+        options: MapOptions(center: l, zoom: zoom, interactive: false),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+          CircleLayerOptions(circles: circleMarkers),
+          new MarkerLayerOptions(markers: [
+            new Marker(
+              point: l,
+              anchorPos: AnchorPos.align(AnchorAlign.center),
+              builder: (ctx) => Container(
+                child: Icon(
+                  Icons.location_on_rounded,
+                  color: Colors.red,
+                  size: 40.0,
+                ),
+              ),
+            )
+          ]),
+        ],
+      ),
+    );
+  }
+
+  Widget uiControlls() {
+    return Column(
+      children: [
+        Container(
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 60.0),
+            child: Column(
+              children: [
+                Text(
+                  'Radius',
+                  textAlign: TextAlign.left,
+                ),
+                Slider(
+                  value: radius.toDouble(),
+                  min: 1000,
+                  max: 30000,
+                  divisions: 30,
+                  label: '${(radius / 1000).round()} KM',
+                  onChanged: (value) {
+                    setState(() {
+                      radius = value;
+                    });
+                  },
+                ),
+              ],
+            )),
+        Container(
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 60.0),
+            child: Column(
+              children: [
+                Text(
+                  'Zoom',
+                  textAlign: TextAlign.left,
+                ),
+                Slider(
+                  value: zoom.toDouble(),
+                  min: 5,
+                  max: 17,
+                  divisions: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      zoom = value;
+                      try {
+                        mc.move(l, zoom);
+                      } catch (e) {
+                        print(e);
+                      }
+                    });
+                  },
+                )
+              ],
+            ))
+      ],
+    );
+  }
+
+  List<Widget> generateChildren() {
+    List<Widget> widgets = [];
+    widgets.add(
+        _permissionGranted == PermissionStatus.granted && _serviceEnabled
+            ? mapWidget()
+            : loader());
+    widgets.add(uiControlls());
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            child: FlutterMap(
-              mapController: mc,
-              options: MapOptions(
-                center: l,
-                zoom: zoom,
-              ),
-              layers: [
-                TileLayerOptions(
-                  urlTemplate:
-                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  subdomains: ['a', 'b', 'c'],
-                ),
-                CircleLayerOptions(circles: circleMarkers),
-                new MarkerLayerOptions(markers: [
-                  new Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: l,
-                    builder: (ctx) => new Container(
-                      child: new FlutterLogo(),
-                    ),
-                  )
-                ]),
-              ],
-            ),
-          ),
-        ),
+        body: Stack(children: generateChildren()),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.save),
+            backgroundColor: Colors.green,
+            onPressed: () {
+              // TODO: call saving method
+              //
+            }),
       ),
     );
   }
